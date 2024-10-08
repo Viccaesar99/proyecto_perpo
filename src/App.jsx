5cd4e2b4c49b63ab4dcf9958eb761033ff94cacd
Victory <victorycesar0880@gmail.com>
2024-10-08 19:00:37 -0400

    Agregar react-router-dom y envonlver la app con el mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Home } from "./components/Pages/Home"
+import { Route, Routes } from "react-router-dom"
+import { Header } from "./components/header/Header.jsx"
+import { Footer } from './components/footer/Footer.jsx'
 
 function App() {
   
 
   return (
-    <main className="w-svw h-svh">
-      <Home />
-    </main>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,14 +1,11 @@
-import { Header } from '../header/Header.jsx'
 import { Body } from '../body/Body.jsx';
-import { Footer } from '../footer/Footer.jsx';
+
 
 export function Home() {
 
     return (
-        <main className='container grid grid-cols-4 border w-full m-auto'>
-            <Header />
+        <main className=''>
             <Body />
-            <Footer />
         </main>
     );
 }
