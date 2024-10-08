430eb5102033299d4e69e542c826dac3de3da750
Victory <victorycesar0880@gmail.com>
2024-10-08 19:49:59 -0400

    Hacer las rutas y hacer que se pueda navegar en ellas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { Home } from "./components/Pages/Home"
+import { About } from "./components/Pages/About.jsx"
+import { Contact } from "./components/Pages/Contact.jsx"
+import { Projects } from "./components/Pages/Projects.jsx"
+import { ErrorPage } from "./components/Pages/ErrorPage.jsx"
+
 import { Route, Routes } from "react-router-dom"
+
 import { Header } from "./components/header/Header.jsx"
 import { Footer } from './components/footer/Footer.jsx'
 
@@ -11,6 +17,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </>
--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,12 +1,21 @@
-import {Boton} from './Boton.jsx'
+import {BotonNav} from './Boton.jsx'
+import { Link } from 'react-router-dom';
 
 export function Nav(){
     return (
         <nav className='w-1/3 flex items-center justify-evenly'>
-            <Boton value="Home"/>
-            <Boton value="Proyectos"/>
-            <Boton value="Sobre mi"/>
-            <Boton value="Contacto"/>
+            <Link className="text-md font-bold font-mono" to="/" >
+                Home
+            </Link>
+            <Link className="text-md font-bold font-mono" to="/projects" >
+                Proyectos
+            </Link>
+            <Link className="text-md font-bold font-mono" to="/about" >
+                Sobre mi
+            </Link>
+            <Link className="text-md font-bold font-mono" to="/contact" >
+                Contacto
+            </Link>
         </nav>
     );
 }
