64591c141bc46ceb8c6a74fc42cf456a46e28c07
Victory <victorycesar0880@gmail.com>
2024-10-09 01:14:34 -0400

    Hacer los links reutilizables, colocarles props

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,20 +1,13 @@
 import { Link } from 'react-router-dom';
+import { BotonNav } from './BotonNav';
 
 export function Nav({styles = "", stylesLink}){
     return (
         <nav className={styles}>
-            <Link className={stylesLink} to="/" >
-                Home
-            </Link>
-            <Link className={stylesLink} to="/projects" >
-                Proyectos
-            </Link>
-            <Link className={stylesLink} to="/about" >
-                Sobre mi
-            </Link>
-            <Link className={stylesLink} to="/contact" >
-                Contacto
-            </Link>
+            <BotonNav value='Home' style={stylesLink}/>
+            <BotonNav value='Proyectos' to='/projects' style={stylesLink}/>
+            <BotonNav value='Sobre mi' to='/about' style={stylesLink}/>
+            <BotonNav value='Contacto' to='/contact' style={stylesLink}/>
         </nav>
     );
 }
